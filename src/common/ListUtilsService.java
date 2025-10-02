aa0732a60b4b9a7ea43d808437a7f29908f39bab
leilaweicman <leilaweic@hotmail.com>
2025-10-02 13:42:48 +0200

    feat(ex2): add Main to show formatNumbers result

--- src/common/ListUtilsService.java
+++ src/common/ListUtilsService.java
@@ -1,6 +1,7 @@
 package common;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListUtilsService {
     public static List<String> filterNamesStartingWithAAndLength3(List<String> names) {
@@ -8,5 +9,15 @@ public class ListUtilsService {
                 .filter(name -> name.startsWith("A"))
                 .filter(name -> name.length() == 3)
                 .toList();
+    }
+
+    public static String formatNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(number -> (isEven(number) ? "e" : "o") + number)
+                .collect(Collectors.joining(", "));
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
--- src/exercise2/Main.java
+++ src/exercise2/Main.java
@@ -1,2 +1,16 @@
-package exercise2;public class Main {
+package exercise2;
+
+import common.ListUtilsService;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> names = Arrays.asList(1, 2, 3, 4, 5);
+
+        String result = ListUtilsService.formatNumbers(names);
+
+        System.out.println("Formatted numbers: " + result);
+    }
 }
