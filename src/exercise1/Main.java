a6b35576f725a5da29118c5673bb38bfc906949d
leilaweicman <leilaweic@hotmail.com>
2025-10-02 13:07:20 +0200

    feat(ex1): add Main with list of names

---
+++ src/exercise1/Main.java
@@ -0,0 +1,12 @@
+package exercise1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Ana", "Ada", "Alex", "Al", "Bob", "Amy");
+
+    }
+}
