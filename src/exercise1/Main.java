93ceca72a443e76fb28edba3e763f0a971f7f61b
leilaweicman <leilaweic@hotmail.com>
2025-10-02 13:17:09 +0200

    feat(ex1): filter names that start with A and have exactly 3 letters

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -8,5 +8,9 @@ public class Main {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Ana", "Ada", "Alex", "Al", "Bob", "Amy");
 
+        System.out.println(names.stream()
+                .filter(name -> name.startsWith("A"))
+                .filter(name -> name.length() == 3)
+                .toList());
     }
 }
