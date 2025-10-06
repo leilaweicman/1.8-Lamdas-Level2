e1343ffe21d805a7d0f1e5c40d0403b1dcfa615b
leilaweicman <leilaweic@hotmail.com>
2025-10-06 10:21:15 +0200

    feat(ex4): add Main with list of items

---
+++ src/exercise4/Main.java
@@ -0,0 +1,14 @@
+package exercise4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> items = Arrays.asList("apple", "banana", "123", "grape", "elephant", "45", "cat");
+
+        System.out.println("Original list: " + items);
+    }
+}
